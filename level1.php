c0f15c3a56b096a7b62d3137fcdada3d078023db
Ahmadou <ah.gueye@laposte.net>
2017-05-15 11:07:05 +0200

    Level
    Niveau 1

---
+++ level1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Monde</title>
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/phaser.min.js" charset="utf-8"></script>
+</head>
+<body>
+
+  <h1>level 1</h1>
+  <div id="lvl1"></div>
+</body>
+<script src="js/phaser.js" charset="utf-8"></script>
+</html>
